0a6058aab1f0c71f5cde691d8cea76b32ed2f01c
Francisco Casalinho <francisco.casalinho@cern.ch>
2025-06-11 11:16:57 +0000

    update

--- Plotter/plot_v10.py
+++ Plotter/plot_v10.py
@@ -9,7 +9,7 @@
 #>>>>Run with --serial option if using py3:
 #   ./plot_v10.py -y 2018 -c mutau --serial
 
-from config.samples_Summer24_v15 import *
+from config.samples_v15 import *
 from TauFW.Plotter.plot.string import filtervars
 from TauFW.Plotter.plot.utils import LOG as PLOG
 from TauFW.Plotter.plot.Plot import Plot, deletehist
