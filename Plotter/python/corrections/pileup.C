977aee614108f268252e6f9cae5c3bb4bb8f3c9c
Francisco Casalinho <francisco.gameiro.casalinho@cern.ch>
2026-01-21 14:52:36 +0100

    update script to calc the puweights on the fly

--- Plotter/python/corrections/pileup.C
+++ Plotter/python/corrections/pileup.C
@@ -1,6 +1,7 @@
 /*
  * @short: provide pileup weights at drawing level
  * @author: Izaak Neutelings (October 2018)
+ * @edit: Francisco Casalinho (January 2026)
  *
  */
 
@@ -13,39 +14,51 @@
 #include "TSystem.h" // for gSystem
 using namespace std;
 TString _datadir_pu = gSystem->ExpandPathName("$CMSSW_BASE/src/TauFW/PicoProducer/data/pileup");
-TString _fname_pu_data = _datadir_pu+"/Data_PileUp_UL2018_69p2.root";
-TString _fname_pu_mc = _datadir_pu+"/MC_PileUp_UL2018.root";
-TH1F* _hist_pu_data;
-TH1F* _hist_pu_mc;
+TString _fname_pu_data = _datadir_pu+"/Data_PileUp_2022_postEE.root";
+TString _fname_pu_mc = _datadir_pu+"/MC_PileUp_2024.root";
 
 
-TH1F* getPUHist(TString filename, TString histname="pileup"){
+vector<float> _pu_data_weights;
+vector<float> _pu_mc_weights;
+
+
+
+vector<float> getPUWeightsFromHist(TString filename, TString histname="pileup"){
   std::cout << ">>> opening " << filename << std::endl;
   TFile *file = new TFile(filename);
   TH1F* hist = (TH1F*) file->Get(histname);
   hist->SetDirectory(0);
   hist->Scale(1./hist->Integral());
+  
+  // Convert histogram to vector
+  vector<float> weights;
+  for(int i = 1; i <= hist->GetNbinsX(); ++i){
+    weights.push_back(hist->GetBinContent(i));
+  }
+  
   file->Close();
-  return hist;
+  return weights;
 }
 
 
-void readPUFile(TString filename_data=_fname_pu_data,TString filename_mc=_fname_pu_mc){
-  _hist_pu_data = getPUHist(filename_data);
-  _hist_pu_mc = getPUHist(filename_mc);
+void readPUFile(TString filename_data=_fname_pu_data, TString filename_mc=_fname_pu_mc){
+  _pu_data_weights = getPUWeightsFromHist(filename_data);
+  _pu_mc_weights = getPUWeightsFromHist(filename_mc);
 }
 
 
 Float_t getPUWeight(Int_t npu){
-  float data = _hist_pu_data->GetBinContent(_hist_pu_data->FindBin(npu));
-  float mc   = _hist_pu_mc->GetBinContent(_hist_pu_mc->FindBin(npu));
-  //std::cout << data << " " << mc << " " << npu << " -> " << data/mc << std::endl;
+  // Bounds checking - ensure npu is within vector range
+  if(npu < 0 || npu >= (Int_t)_pu_data_weights.size() || npu >= (Int_t)_pu_mc_weights.size()){
+    return 1.0;
+  }
+  
+  float data = _pu_data_weights[npu];
+  float mc   = _pu_mc_weights[npu];
+  
   if(mc > 0.){
     return data/mc;
-    //std::cout << "pu weight =" << data/mc << std::endl;
-  }//else{
-    //std::cout << "No predefined pileup weights" << std::endl;
-  //  }
+  }
   return 1.0;
 }
 
@@ -53,4 +66,3 @@ Float_t getPUWeight(Int_t npu){
 void pileup(){
   std::cout << ">>> initializing pileup.C ... " << std::endl;
 }
-
